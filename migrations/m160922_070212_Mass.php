03ebc8ad83a2af74e56d77d3a6e0bbac85df878c
Trishkin Vadim <halumein@gmail.com>
2016-09-25 14:50:14 +0500

    фикс миграций после мержа

--- migrations/m160922_070212_Mass.php
+++ migrations/m160922_070212_Mass.php
@@ -15,41 +15,27 @@ class m160922_070212_Mass extends Migration
     public function safeUp()
     {
         $tableOptions = 'ENGINE=InnoDB';
-        $transaction=$this->db->beginTransaction();
-        try{
-             $this->createTable('{{%cashbox_operation}}',[
-               'id'=> $this->primaryKey(11),
-               'type'=> $this->string()->notNull(),
-               'balance'=> $this->decimal(2, 11)->notNull(),
-               'sum'=> $this->decimal(2, 11)->notNull(),
-               'cashbox_id'=> $this->integer(11)->notNull(),
-               'model'=> $this->string(255)->null()->defaultValue(null),
-               'item_id'=> $this->integer(11)->null()->defaultValue(null),
-               'date'=> $this->dateTime()->notNull(),
-               'client_id'=> $this->integer(11)->null()->defaultValue(null),
-               'staffer_id'=> $this->integer(11)->notNull(),
-               'comment'=> $this->text()->null()->defaultValue(null),
-               'status'=> $this->string()->notNull()->defaultValue('created'),
-            ], $tableOptions);
-            $this->createIndex('cashbox_id','{{%cashbox_operation}}','cashbox_id',true);
-            $this->addForeignKey('fk_cashbox_operation_cashbox_id','{{%cashbox_operation}}','cashbox_id','cashbox_cashbox','id');
-            $transaction->commit();
-        } catch (Exception $e) {
-             echo 'Catch Exception '.$e->getMessage().' and rollBack this';
-             $transaction->rollBack();
-        }
+         $this->createTable('{{%cashbox_operation}}',[
+           'id'=> $this->primaryKey(11),
+           'type'=> $this->string()->notNull(),
+           'balance'=> $this->decimal(11, 2)->notNull(),
+           'sum'=> $this->decimal(11, 2)->notNull(),
+           'cashbox_id'=> $this->integer(11)->notNull(),
+           'model'=> $this->string(255)->null()->defaultValue(null),
+           'item_id'=> $this->integer(11)->null()->defaultValue(null),
+           'date'=> $this->dateTime()->notNull(),
+           'client_id'=> $this->integer(11)->null()->defaultValue(null),
+           'staffer_id'=> $this->integer(11)->notNull(),
+           'comment'=> $this->text()->null()->defaultValue(null),
+           'status'=> $this->string()->notNull()->defaultValue('created'),
+        ], $tableOptions);
+        $this->createIndex('cashbox_id','{{%cashbox_operation}}','cashbox_id',true);
+        $this->addForeignKey('fk_cashbox_operation_cashbox_id','{{%cashbox_operation}}','cashbox_id','cashbox_cashbox','id');
     }
 
     public function safeDown()
     {
-        $transaction=$this->db->beginTransaction();
-        try{
-            $this->dropForeignKey('fk_cashbox_operation_cashbox_id', '{{%cashbox_operation}}');
-            $this->dropTable('{{%cashbox_operation}}');
-            $transaction->commit();
-        } catch (Exception $e) {
-            echo 'Catch Exception '.$e->getMessage().' and rollBack this';
-            $transaction->rollBack();
-        }
+        $this->dropForeignKey('fk_cashbox_operation_cashbox_id', '{{%cashbox_operation}}');
+        $this->dropTable('{{%cashbox_operation}}');
     }
 }
