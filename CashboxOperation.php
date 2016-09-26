5e6983c5b5d25e64a0f03cf58e2988cd63099124
Trishkin Vadim <halumein@gmail.com>
2016-09-26 18:01:27 +0500

    fastfix

--- CashboxOperation.php
+++ CashboxOperation.php
@@ -23,37 +23,6 @@ class CashboxOperation extends Component
         if ($model->type === 'outcome') {
             $model->balance = $cashBox->balance - $model->sum;
         }
-        
-        if ($model->save()) {
-            $cashBox->balance = $model->balance;
-            $cashBox->save();
-
-            return [
-                'status' => true
-            ];
-        } else {
-            return [
-                'status' => false,
-                'message' => $model->errors
-            ];
-        }
-    }
-
-    public function addTransaction($params)
-    {
-        $model = new Operation();
-        $model->load($params);
-        $model->date = date('Y:m:d H:i:s');
-        $model->staffer_id = \Yii::$app->user->id;
-        $cashBox = Cashbox::findOne($model->cashbox_id);
-
-        if ($model->type === 'income') {
-            $model->balance =  $cashBox->balance + $model->sum;
-        }
-
-        if ($model->type === 'outcome') {
-            $model->balance = $cashBox->balance - $model->sum;
-        }
 
         if ($model->save()) {
             $cashBox->balance = $model->balance;
@@ -69,5 +38,6 @@ class CashboxOperation extends Component
             ];
         }
     }
+
 
 }
