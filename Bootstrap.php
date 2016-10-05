4016ed577946723137f2b7165309de2dbe37ae26
Ilya <php@php.name>
2016-10-05 19:10:48 +0500

    fix c => C

--- Bootstrap.php
+++ Bootstrap.php
@@ -9,7 +9,7 @@ class Bootstrap implements BootstrapInterface
     public function bootstrap($app)
     {
         if(!$app->has('cashbox')) {
-            $app->set('cashbox', ['class' => 'halumein\cashbox\cashbox']);
+            $app->set('cashbox', ['class' => 'halumein\cashbox\Cashbox']);
         }
     }
 }
