832f519b938e89e0dd7ac9063d0ef276d592abea
Ilya <php@php.name>
2016-12-12 15:53:02 +0500

    total in report

--- widgets/views/reportBalanceByPeriod.php
+++ widgets/views/reportBalanceByPeriod.php
@@ -5,6 +5,10 @@ $date = $dateStop;
 if(!$date) {
     $date = date('Y-m-d H:i:s');
 }
+
+$in = 0;
+$out = 0;
+$sum = 0;
 ?>
 <div class="row">
     <div class="col-sm-12">
@@ -32,6 +36,7 @@ if(!$date) {
                         <td>
                             <?php
                             $income = \Yii::$app->cashbox->getIncomeSumByPeriod($dateStart, $date, $cashbox->id);
+                            $in+=$income;
                             echo "<a href=\"" . Url::toRoute(['/cashbox/operation/index', 'date_start' => $dateStart, 'date_stop' => $date, 'OperationSearch' => ['cashbox_id' => $cashbox->id, 'type' => 'income']]) . "\">".(int)$income."</a>";
                             ?>
                         </td>
@@ -39,19 +44,35 @@ if(!$date) {
                             
                             <?php
                             $outcome = \Yii::$app->cashbox->getOutcomeSumByPeriod($dateStart, $date, $cashbox->id);
+                            $out+=$outcome;
                             echo "<a href=\"" . Url::toRoute(['/cashbox/operation/index', 'date_start' => $dateStart, 'date_stop' => $date, 'OperationSearch' => ['cashbox_id' => $cashbox->id, 'type' => 'outcome']]) . "\">".(int)$outcome."</a>";
                             ?>
                         </td>
                         <td>
                             <?php
                             $balance = \Yii::$app->cashbox->getBalanceByDate($date, $cashbox->id);
+                            $sum+=(int)$balance;
                             echo "<a href=\"" . Url::toRoute(['/cashbox/operation/index', 'date_start' => $dateStart, 'date_stop' => $date, 'OperationSearch' => ['cashbox_id' => $cashbox->id]]) . "\">".(int)$balance."</a>";
                             ?>
-                            
                         </td>
                     </tr>
                 <?php } ?>
             </tbody>
+            
+            <tfoot>
+                <th>
+                    Итого:
+                </th>
+                <th>
+                    <?=$in;?>
+                </th>
+                <th>
+                    <?=$out;?>
+                </th>
+                <th>
+                    <?=$sum;?>
+                </th>
+            </tfoot>
         </table>
     </div>
 </div>
