bf33e33dc38e01f873ad52ecb0707f6e23bed62c
Trishkin Vadim <halumein@gmail.com>
2017-03-04 14:27:39 +0500

    test

--- widgets/RepaymentForm.php
+++ widgets/RepaymentForm.php
@@ -48,7 +48,9 @@ class RepaymentForm extends \yii\base\Widget
         $operationModel = new Operation();
         $cashboxes = Cashbox::getAvailable();
 
-        $operationModel->cashbox_id = \Yii::$app->user->identity->defaultCashbox;
+        $userModel = Yii::$app->getModule('cashbox')->userModel;
+
+        $operationModel->cashbox_id = $userModel->defaultCashbox;
 
         if ($paymentTypeToCashbox = Yii::$app->getModule('cashbox')->paymentTypeToCashbox) {
             if (isset($paymentTypeToCashbox[$this->order->payment_type_id])) {
